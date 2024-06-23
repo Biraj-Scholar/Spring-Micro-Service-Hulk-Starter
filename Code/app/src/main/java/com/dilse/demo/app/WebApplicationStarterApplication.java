543dc6af013661220a35eab8391628e21cf66bf4
Biraj-Scholar <103089978+Biraj-Scholar@users.noreply.github.com>
2024-06-23 19:05:36 +0530

    Init
    Init Code for Micro service

---
+++ Code/app/src/main/java/com/dilse/demo/app/WebApplicationStarterApplication.java
@@ -0,0 +1,13 @@
+package com.dilse.demo.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplicationStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApplicationStarterApplication.class, args);
+	}
+
+}
---
+++ Code/app/src/test/java/com/dilse/demo/app/WebApplicationStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dilse.demo.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
